d72deab1d10340316f0ae011b7b4022bb9bb818e
Чубреич Ярослав Сергеевич <yaroslavtchubreitch@yandex.ru>
2026-03-10 18:44:06 +0700

    Update Form.js

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -5,9 +5,6 @@ import axios from 'axios';
 const Form = () => {
     const navigate = useNavigate();
     
-    const BIN_ID = '69ad45e2ae596e708f6c5416';
-    const API_KEY = '$2a$10$RXqthTfr5oH0p4Knha6oe.A64Sl1yetsQy.rzh8qGjAlFV/w1pihu';
-    
     const [formData, setFormData] = useState({
         title: '',
         description: '',
@@ -27,20 +24,7 @@ const Form = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const current = await axios.get(
-                `https://api.jsonbin.io/v3/b/${BIN_ID}/latest`,
-                { headers: { 'X-Master-Key': API_KEY } }
-            );
-            
-            const newIncident = { ...formData, id: Date.now().toString() };
-            const updated = [...current.data.record.incidents, newIncident];
-            
-            await axios.put(
-                `https://api.jsonbin.io/v3/b/${BIN_ID}`,
-                { incidents: updated },
-                { headers: { 'X-Master-Key': API_KEY } }
-            );
-            
+            await axios.post('http://localhost:5000/incidents', formData);
             alert('Запись успешно добавлена!');
             navigate('/');
         } catch (error) {
